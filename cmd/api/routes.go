389b0b695943001726191addb752f5d796b91390
Merakigai <merakigailabs@gmail.com>
2024-12-28 12:31:00 +0700

    ch14: generate auth token and add login handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,6 +26,7 @@ func (app *application) routes() http.Handler {
 	//		Users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	// Return the httprouter instance.
 	return app.recoverPanic(app.rateLimit(router))
--- internal/data/users.go
+++ internal/data/users.go
@@ -19,6 +19,8 @@ type UserModel struct {
 	DB *sql.DB
 }
 
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -209,4 +211,8 @@ func (m UserModel) GetForToken(tokenScope, tokenPlaintext string) (*User, error)
 	}
 
 	return &user, nil
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
